6436d0860d0ab22fc6f6c84ab469006cf26fca91
DLaptiev <akiosarkiz@gmail.com>
2024-07-26 00:06:16 +0200

    update project files

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'mangaclub_frontend',
+      name: 'mangaclub-frontend',
       cwd: '/var/www/mangaclub-frontend/current',
       script: 'npm',
       args: 'run start',
