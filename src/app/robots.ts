139f123827bb6de751605508e18dc6593b914c33
DLaptiev <akiosarkiz@gmail.com>
2024-08-05 01:10:42 +0200

    update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,18 +1,11 @@
 import { MetadataRoute } from 'next';
-import urlJoin from 'url-join';
 
 export default function robots(): MetadataRoute.Robots {
-  if (!process.env.NEXT_PUBLIC_APP_URL) {
-    throw new Error('NEXT_PUBLIC_APP_URL is not defined');
-  }
-
-  const sitemapUrl = urlJoin(process.env.NEXT_PUBLIC_APP_URL, '/sitemap-index.xml');
-
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '',
     },
-    sitemap: sitemapUrl,
   };
 }
