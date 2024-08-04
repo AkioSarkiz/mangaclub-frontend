4bf6a1ddfd16fcf135611fe21a7ce78715f12c4a
DLaptiev <akiosarkiz@gmail.com>
2024-08-04 14:59:10 +0200

    add robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+import urlJoin from 'url-join';
+
+export default function robots(): MetadataRoute.Robots {
+  if (!process.env.NEXT_PUBLIC_BACKEND_URL) {
+    throw new Error('NEXT_PUBLIC_BACKEND_URL is not defined');
+  }
+
+  const sitemapUrl = urlJoin(process.env.NEXT_PUBLIC_BACKEND_URL, '/sitemap.xml');
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: sitemapUrl,
+  };
+}
