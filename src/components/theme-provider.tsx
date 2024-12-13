9d70a0dc5d339f84d4cfee14e9f2497c4c3aecf6
DLaptiev <akiosarkiz@gmail.com>
2024-12-13 10:38:29 +0100

    fix: next-themes typescript issue

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  // @ts-ignore
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
